030647079c4d6acd16d60ddaf77a85dc8587aecf
Lise Andersen <lise2895@edu.campusvejle.dk>
2017-06-15 13:27:36 +0200

    change from bootstrap to flexbox grid, added base of index

--- index.php
+++ index.php
@@ -2,18 +2,51 @@
 <html lang="en">
 
 <head>
+    <!--
+    WILDERNESS by Lise Andersen
+    PLEASE DO NOT REMOVE THE CREDIT OR REDISTRIBUTE AS YOUR OWN!
+    -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Wilderness | Index</title>
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="css/styles.css" rel="stylesheet"> </head>
+    <!-- Stylesheet -->
+    <link href="css/styles.css" rel="stylesheet">
+    <!-- HTML5 Boilerplates normalize -->
+    <link href="css/normalize.css" rel="stylesheet">
+    <!-- FlexBox Grid -->
+    <link href="css/flexboxgrid.css" rel="stylesheet">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css"> </head>
 
 <body>
+    <!-- header start -->
+    <header><img src="images/header_img.jpg" alt="header"></header>
+    <!-- header slut -->
+    <nav>
+        <ul>
+            <li>home</li>
+            <li>planter</li>
+            <li>indretning</li>
+            <li>log ind</li>
+        </ul>
+    </nav>
+    <main>
+        <article>
+            <h2></h2><img src="images/laenestol.jpg" alt="laenestol-article-img"></article>
+        <article>
+            <h2></h2><img src="images/mit_plante_eksperiment.jpg" alt="plante-eksperiment-article-img"></article>
+        <article>
+            <h2></h2><img src="images/urban_jungle_make-over.jpg" alt="urban-jungle-article-img"></article>
+        <aside><img src="" alt="">
+            <p></p>
+            <hr>
+            <!-- instagram, facebook, flickr, linkedIn ikoner her -->
+        </aside>
+    </main>
+    <footer>&copy; 2017 - wilderness</footer>
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-    <!-- Bootstrap Core JavaScript -->
+    <!-- JavaScript (for Bootstrap) -->
     <script src="js/bootstrap.min.js"></script>
 </body>
 
