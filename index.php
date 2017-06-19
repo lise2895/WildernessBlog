5a31da6f977add5f91286f3757b1a53af7803732
Lise Andersen <lise2895@edu.campusvejle.dk>
2017-06-19 14:13:36 +0200

    index styled and complete

--- index.php
+++ index.php
@@ -22,14 +22,13 @@
     <!-- header start -->
     <div class="main-container">
         <div class="container">
-            <div class="row center col-xs-12">
+            <div class="row center-xs col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <header><img src="images/header_img.jpg" alt="header"></header>
             </div>
         </div>
         <!-- header end -->
         <!-- navigation start -->
-        <div class="row-center col-xs-12">
-            <hr>
+        <div class="row-center col-xs-12 nav-horizon">
             <nav>
                 <ul>
                     <li><a href="">home</a></li>
@@ -38,54 +37,57 @@
                     <li class="login"><a href="">log ind</a></li>
                 </ul>
             </nav>
-            <hr> </div>
+        </div>
         <!-- navigation end -->
         <!-- main content start -->
         <main>
-            <div class="container">
-                <div class="row">
-                    <!-- sidebar start -->
-                    <div class="col-xs-3">
-                        <aside><img src="images/frk_overspringshandling.jpg" alt="sidebar-img">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repudiandae autem ipsa incidunt sed molestias. Veritatis deleniti ad minima facere dolorum animi earum, in atque molestiae pariatur, odit dolores corporis!</p>
-                            <hr>
-                            <!-- social media icons start --><i class="fa fa-instagram" aria-hidden="true"></i> <i class="fa fa-facebook-square" aria-hidden="true"></i><i class="fa fa-flickr" aria-hidden="true"></i> <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-                            <!-- social media icons end -->
-                        </aside>
-                    </div>
-                    <!-- sidebar end -->
-                    <div class="col-xs-9">
-                        <!-- static articles start -->
-                        <article>
-                            <h2>vores nye lænestol i egetræ og kernelæder</h2><img src="images/laenestol.jpg" alt="laenestol-article-img">
-                            <p>
-                                <!-- poster & day of posting -->
-                            </p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
-                            <hr> </article>
-                        <article>
-                            <h2>mit plante eksperiment</h2><img src="images/mit_plante_eksperiment.jpg" alt="plante-eksperiment-article-img">
-                            <p>
-                                <!-- poster & day of posting -->
-                            </p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
-                            <hr> </article>
-                        <article>
-                            <h2>min urban jungle baggårds make-over</h2><img src="images/urban_jungle_make-over.jpg" alt="urban-jungle-article-img">
-                            <p>
-                                <!-- poster & day of posting -->
-                            </p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
-                        </article>
-                    </div>
-                    <!-- static articles end -->
+            <p>
+                <!-- space between nav and main -->
+            </p>
+            <div class="row">
+                <!-- sidebar start -->
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 center-xs">
+                    <aside class="start-xs"> <img src="images/frk_overspringshandling.jpg" alt="sidebar-img">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repudiandae autem ipsa incidunt sed molestias. Veritatis deleniti ad minima facere dolorum animi earum, in atque molestiae pariatur, odit dolores corporis!</p>
+                        <hr>
+                        <!-- social media icons start --><i class="fa fa-instagram fa-lg"></i> <i class="fa fa-facebook-square fa-lg"></i><i class="fa fa-flickr fa-lg"></i> <i class="fa fa-linkedin-square fa-lg"></i>
+                        <!-- social media icons end -->
+                    </aside>
                 </div>
+                <!-- sidebar end -->
+                <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 center-xs">
+                    <!-- static articles start -->
+                    <article class="start-xs">
+                        <h2>vores nye lænestol i egetræ og kernelæder</h2><img src="images/laenestol.jpg" alt="laenestol-article-img">
+                        <p>
+                            <!-- poster & day of posting -->
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
+                        <hr> </article>
+                    <article class="start-xs">
+                        <h2>mit plante eksperiment</h2><img src="images/mit_plante_eksperiment.jpg" alt="plante-eksperiment-article-img">
+                        <p>
+                            <!-- poster & day of posting -->
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
+                        <hr> </article>
+                    <article class="start-xs">
+                        <h2>min urban jungle baggårds make-over</h2><img src="images/urban_jungle_make-over.jpg" alt="urban-jungle-article-img">
+                        <p>
+                            <!-- poster & day of posting -->
+                        </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
+                    </article>
+                </div>
+                <!-- static articles end -->
             </div>
         </main>
         <!-- main content end -->
         <!-- footer start -->
         <footer>
-            <hr> &copy; 2017 - wilderness</footer>
+            <hr>
+            <p>&copy; 2017 - wilderness</p>
+        </footer>
     </div>
     <!-- footer end -->
 </body>
