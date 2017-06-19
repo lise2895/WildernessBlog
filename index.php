db6a76c1a4df842a0a1daece54005d48ec0a48f9
Lise Andersen <lise2895@edu.campusvejle.dk>
2017-06-19 10:45:59 +0200

    basic layout #1

--- index.php
+++ index.php
@@ -9,67 +9,84 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Wilderness | Index</title>
-    <!-- Stylesheet -->
-    <link href="css/styles.css" rel="stylesheet">
     <!-- HTML5 Boilerplates normalize -->
     <link href="css/normalize.css" rel="stylesheet">
     <!-- FlexBox Grid -->
     <link href="css/flexboxgrid.css" rel="stylesheet">
     <!-- FontAwesome -->
-    <link rel="stylesheet" href="css/font-awesome.min.css"> </head>
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Stylesheet -->
+    <link href="css/styles.css" rel="stylesheet"> </head>
 
 <body>
     <!-- header start -->
-    <header><img src="images/header_img.jpg" alt="header"></header>
-    <!-- header end -->
-    <!-- navigation start -->
-    <nav>
-        <ul>
-            <li>home</li>
-            <li>planter</li>
-            <li>indretning</li>
-            <li>log ind</li>
-        </ul>
-    </nav>
-    <!-- navigation end -->
-    <!-- main content start -->
-    <main>
-        <!-- static articles start -->
-        <article>
-            <h2></h2><img src="images/laenestol.jpg" alt="laenestol-article-img">
-            <p>
-                <!-- poster & day of posting -->
-            </p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
-            <hr> </article>
-        <article>
-            <h2></h2><img src="images/mit_plante_eksperiment.jpg" alt="plante-eksperiment-article-img">
-            <p>
-                <!-- poster & day of posting -->
-            </p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
-            <hr> </article>
-        <article>
-            <h2></h2><img src="images/urban_jungle_make-over.jpg" alt="urban-jungle-article-img">
-            <p>
-                <!-- poster & day of posting -->
-            </p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
-        </article>
-        <!-- static articles end -->
-        <!-- sidebar start -->
-        <aside><img src="images/frk_overspringshandling.jpg" alt="sidebar-img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repudiandae autem ipsa incidunt sed molestias. Veritatis deleniti ad minima facere dolorum animi earum, in atque molestiae pariatur, odit dolores corporis!</p>
+    <div class="main-container">
+        <div class="container">
+            <div class="row center col-xs-12">
+                <header><img src="images/header_img.jpg" alt="header"></header>
+            </div>
+        </div>
+        <!-- header end -->
+        <!-- navigation start -->
+        <div class="row-center col-xs-12">
             <hr>
-            <!-- social media icons start --><i class="fa fa-instagram" aria-hidden="true"></i> <i class="fa fa-facebook-square" aria-hidden="true"></i><i class="fa fa-flickr" aria-hidden="true"></i> <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            <!-- social media icons end -->
-        </aside>
-        <!-- sidebar end -->
-    </main>
-    <!-- main content end -->
-    <!-- footer start -->
-    <footer>
-        <hr> &copy; 2017 - wilderness</footer>
+            <nav>
+                <ul>
+                    <li><a href="">home</a></li>
+                    <li><a href="">planter</a></li>
+                    <li><a href="">indretning</a></li>
+                    <li class="login"><a href="">log ind</a></li>
+                </ul>
+            </nav>
+            <hr> </div>
+        <!-- navigation end -->
+        <!-- main content start -->
+        <main>
+            <div class="container">
+                <div class="row">
+                    <!-- sidebar start -->
+                    <div class="col-xs-3">
+                        <aside><img src="images/frk_overspringshandling.jpg" alt="sidebar-img">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repudiandae autem ipsa incidunt sed molestias. Veritatis deleniti ad minima facere dolorum animi earum, in atque molestiae pariatur, odit dolores corporis!</p>
+                            <hr>
+                            <!-- social media icons start --><i class="fa fa-instagram" aria-hidden="true"></i> <i class="fa fa-facebook-square" aria-hidden="true"></i><i class="fa fa-flickr" aria-hidden="true"></i> <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                            <!-- social media icons end -->
+                        </aside>
+                    </div>
+                    <!-- sidebar end -->
+                    <div class="col-xs-9">
+                        <!-- static articles start -->
+                        <article>
+                            <h2>vores nye lænestol i egetræ og kernelæder</h2><img src="images/laenestol.jpg" alt="laenestol-article-img">
+                            <p>
+                                <!-- poster & day of posting -->
+                            </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
+                            <hr> </article>
+                        <article>
+                            <h2>mit plante eksperiment</h2><img src="images/mit_plante_eksperiment.jpg" alt="plante-eksperiment-article-img">
+                            <p>
+                                <!-- poster & day of posting -->
+                            </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
+                            <hr> </article>
+                        <article>
+                            <h2>min urban jungle baggårds make-over</h2><img src="images/urban_jungle_make-over.jpg" alt="urban-jungle-article-img">
+                            <p>
+                                <!-- poster & day of posting -->
+                            </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio exercitationem pariatur fuga suscipit natus distinctio molestiae, ab reiciendis error dolor recusandae voluptatibus eaque, at nulla ut, molestias vero animi sapiente.</p>
+                        </article>
+                    </div>
+                    <!-- static articles end -->
+                </div>
+            </div>
+        </main>
+        <!-- main content end -->
+        <!-- footer start -->
+        <footer>
+            <hr> &copy; 2017 - wilderness</footer>
+    </div>
     <!-- footer end -->
 </body>
 
